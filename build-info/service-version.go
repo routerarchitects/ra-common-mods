8a854c3534fa6306740881f56a14ecf07a439e6a
uttam21router <uttam21.vashisth@routerarchitects.com>
2026-03-31 17:45:34 +0530

    added CommitHAsh via debug package

--- build-info/service-version.go
+++ build-info/service-version.go
@@ -1,5 +1,48 @@
 package build_info
 
-var Version = ""
-var BuildTimestamp = ""
-var CommitHash = ""
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+var version = ""
+var buildTimestamp = ""
+var commitHash = ""
+
+func getVersion() string {
+	return version
+}
+
+func GetBuildTimestamp() string {
+	return buildTimestamp
+}
+
+func getCommitHash() string {
+	return commitHash
+}
+
+func makeCommitHash() string {
+	commitHash := getCommitHash()
+	if commitHash == "" {
+		info, ok := debug.ReadBuildInfo()
+		if ok {
+			for _, s := range info.Settings {
+				if s.Key == "vcs.revision" {
+					commitHash = s.Value
+				}
+			}
+		}
+	}
+	return commitHash
+}
+
+func GetFullVersion() string {
+	version := getVersion()
+
+	if version == "" {
+		return makeCommitHash()
+	} else {
+		return fmt.Sprintf("%s-%s", version, makeCommitHash())
+	}
+
+}
