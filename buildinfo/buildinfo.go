afd9dbfd5f7eb9a17c4c54f9f465107a29ecd01c
uttam21router <uttam21.vashisth@routerarchitects.com>
2026-04-01 16:48:08 +0530

    Service Versioning (#13)
    * [#11] Service Versioning
    
    * added buildinfo module

---
+++ buildinfo/buildinfo.go
@@ -0,0 +1,31 @@
+package buildinfo
+
+import (
+	"runtime/debug"
+)
+
+var version = ""
+var buildTimestamp = ""
+var commitHash = ""
+
+func GetVersion() string {
+	return version
+}
+
+func GetBuildTimestamp() string {
+	return buildTimestamp
+}
+
+func GetCommitHash() string {
+	if commitHash == "" {
+		info, ok := debug.ReadBuildInfo()
+		if ok {
+			for _, s := range info.Settings {
+				if s.Key == "vcs.revision" {
+					return s.Value
+				}
+			}
+		}
+	}
+	return commitHash
+}
---
+++ buildinfo/go.mod
@@ -0,0 +1,3 @@
+module github.com/routerarchitects/ra-common-mods/buildinfo
+
+go 1.25.0
